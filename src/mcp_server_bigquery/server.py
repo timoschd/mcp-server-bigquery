399e17821885f9bf58c613c44aabf75b69295124
ViduraErandika <vidura.karunarathna@ncinga.net>
2025-04-10 18:13:14 +0530

    added google cloud svc account key for bigquery

--- src/mcp_server_bigquery/server.py
+++ src/mcp_server_bigquery/server.py
@@ -1,4 +1,5 @@
 from google.cloud import bigquery
+from google.oauth2 import service_account
 import logging
 from mcp.server.models import InitializationOptions
 import mcp.types as types
@@ -26,14 +27,25 @@ logger.setLevel(logging.DEBUG)
 logger.info("Starting MCP BigQuery Server")
 
 class BigQueryDatabase:
-    def __init__(self, project: str, location: str, datasets_filter: list[str]):
+    def __init__(self, project: str, location: str, key_file: str, datasets_filter: list[str]):
         """Initialize a BigQuery database client"""
         if not project:
             raise ValueError("Project is required")
         if not location:
             raise ValueError("Location is required")
-
-        self.client = bigquery.Client(project=project, location=location)
+        if not key_file:
+            self.client = bigquery.Client(project=project, location=location)
+        else:
+            try:
+                credentials_path = key_file
+                credentials = service_account.Credentials.from_service_account_file(
+                    credentials_path,
+                    scopes=["https://www.googleapis.com/auth/cloud-platform"],
+                )
+            except Exception as e:
+                logger.error(f"Error loading service account credentials: {e}")
+                raise ValueError(f"Invalid key file: {e}")
+            self.client = bigquery.Client(credentials=credentials, project=project, location=location)
         self.datasets_filter = datasets_filter
 
     def execute_query(self, query: str, params: dict[str, Any] | None = None) -> list[dict[str, Any]]:
@@ -94,10 +106,10 @@ class BigQueryDatabase:
             bigquery.ScalarQueryParameter("table_name", "STRING", table_id),
         ])
 
-async def main(project: str, location: str, datasets_filter: list[str]):
+async def main(project: str, location: str, key_file:str, datasets_filter: list[str]):
     logger.info(f"Starting BigQuery MCP Server with project: {project} and location: {location}")
 
-    db = BigQueryDatabase(project, location, datasets_filter)
+    db = BigQueryDatabase(project, location, key_file, datasets_filter)
     server = Server("bigquery-manager")
 
     # Register handlers
